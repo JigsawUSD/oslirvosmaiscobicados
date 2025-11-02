66793656ddf832fa917781725268aaa365578e56
NINJA GAME -YT <srclash98@gmail.com>
2025-11-02 15:32:31 +0000

    https://livrariascuritiba.vteximg.com.br/arquivos/ids/2021469/LV316499.j

--- next.config.ts
+++ next.config.ts
@@ -52,6 +52,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'livrariascuritiba.vteximg.com.br',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
