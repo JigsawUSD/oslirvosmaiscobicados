8a606fc1f90175f0e7de689811b5252f44b52c08
NINJA GAME -YT <srclash98@gmail.com>
2025-11-02 15:29:54 +0000

    https://0.academia-photos.com/attachment_thumbnails/110574608/mini_magic

--- next.config.ts
+++ next.config.ts
@@ -46,6 +46,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: '0.academia-photos.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
