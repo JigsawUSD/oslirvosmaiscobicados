1a8129905dd9061121951c4f4431d4c0404506f7
NINJA GAME -YT <srclash98@gmail.com>
2025-11-17 03:50:38 +0000

    https://zeoob.com/generated-images/zeoob.com_0cgyvtldw9_photo.png link d

--- next.config.ts
+++ next.config.ts
@@ -82,6 +82,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'zeoob.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
